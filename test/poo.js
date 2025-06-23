09cb4a34ac92ac8146d9a834a42e0a7915757426
03matig <matiasgarinav03@gmail.com>
2025-06-23 18:18:38 -0400

    First Commit reverted

--- test/poo.js
+++
@@ -1,6 +0,0 @@
-let nombres = {
-    nombre: "Juan",
-    apellido: "Pérez"
-}
-console.log("El nombre del usuario es: ", nombres.nombre, ", mientras que su apellido es: ", nombres.apellido);
-console.log("El nombre completo del usuario es: ", nombres.nombre + " " + nombres.apellido);
